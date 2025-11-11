878ad4820c3a804fc51729ddca7f3015767b474b
XooWI <ehvaatihon@gmail.com>
2025-11-11 18:11:28 +0300

    Создана основа бота, получение API и информация об API

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/XooWI/BenefitBot/internal/utils"
+)
+
+func main(){
+	api, err := utils.GetAPI()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	info, err := api.Bots.GetBot(ctx)
+	fmt.Printf("Get me: %#v %#v", info, err)
+}
